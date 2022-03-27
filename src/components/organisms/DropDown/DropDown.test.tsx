e55c0c7a7eb1619f127ef65c00c3f8cf60ec53c5
Shubham-zemoso <shubham.kukreti@zemosolabs.com>
2022-03-27 20:47:43 +0530

    remove code smells

--- src/components/organisms/DropDown/DropDown.test.tsx
+++ src/components/organisms/DropDown/DropDown.test.tsx
@@ -3,7 +3,9 @@ import { render } from "@testing-library/react";
 import "@testing-library/jest-dom";
 import DropDown from "./DropDown";
 
-const handleClose = () => {};
+const handleClose = () => {
+	console.log("Handle Close");
+};
 
 it("checks rendering of component", () => {
 	render(<DropDown handleClose={handleClose} anchorEl={null} />);
