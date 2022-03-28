080998c3f408aad20c0f81d5882858c8553ccb1f
Shubham-zemoso <shubham.kukreti@zemosolabs.com>
2022-03-28 16:56:40 +0530

    added Auth0 to app

--- src/index.tsx
+++ src/index.tsx
@@ -5,11 +5,19 @@ import App from "./App";
 import reportWebVitals from "./reportWebVitals";
 import { Provider } from "react-redux";
 import { store } from "./store/store";
+import { Auth0Provider } from "@auth0/auth0-react";
 
 ReactDOM.render(
-	<Provider store={store}>
-		<App />
-	</Provider>,
+	<Auth0Provider
+		domain="dev-waah902s.us.auth0.com"
+		clientId="xH4r92yMngCaI5RW1y3G5kciTLxlasqJ"
+		redirectUri={window.location.origin}
+	>
+		<Provider store={store}>
+			<App />
+		</Provider>
+	</Auth0Provider>,
+
 	document.getElementById("root")
 );
 
